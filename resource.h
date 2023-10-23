a12dbabf28e5d2e5d246fe056ffb2fde4f2418af
권윤호 <yoonhoktx@gmail.com>
2023-10-23 10:35:11 +0900

    초기 커밋.

---
+++ resource.h
@@ -0,0 +1,26 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// FV.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_FV_DIALOG                   102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       131
+#define ID_FILE_MESSAGEBOX              32771
+#define ID_FILE_DRAWREC                 32772
+#define ID_SELECT_SIZE                  32773
+#define ID_SELECT_COLOR                 32774
+#define ID_FILE_DRAWCIRCLE              32775
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
